7b1da37bc28770c03784080e660469116d175ec1
fgsfds <pvt.fgsfds@gmail.com>
2019-05-07 01:22:02 +0300

    X axis comes before Y, at least on Switch

---
+++ src/posix/nx/i_swkbd.cpp
@@ -0,0 +1,59 @@
+/*
+** i_swkbd.cpp
+** Handles Switch on-screen keyboard
+**
+**---------------------------------------------------------------------------
+** Copyright 2013-2016 Randy Heit
+** Copyright 2016 Christoph Oelckers
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+** 1. Redistributions of source code must retain the above copyright
+**	notice, this list of conditions and the following disclaimer.
+** 2. Redistributions in binary form must reproduce the above copyright
+**	notice, this list of conditions and the following disclaimer in the
+**	documentation and/or other materials provided with the distribution.
+** 3. The name of the author may not be used to endorse or promote products
+**	derived from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**---------------------------------------------------------------------------
+**
+*/
+
+#include <switch.h>
+
+#include "i_system.h"
+#include "cmdlib.h"
+#include "doomerrors.h"
+
+bool I_OnScreenKeyboard(const char *hint, char *out, int outlen)
+{
+	if (!out || !outlen) return false;
+
+	SwkbdConfig kbd;
+	Result rc = swkbdCreate(&kbd, 0);
+	if (R_FAILED(rc)) return false;
+
+	swkbdConfigMakePresetDefault(&kbd);
+	swkbdConfigSetHeaderText(&kbd, hint);
+	swkbdConfigSetGuideText(&kbd, hint);
+	swkbdConfigSetInitialText(&kbd, out);
+
+	rc = swkbdShow(&kbd, out, outlen-1);
+	swkbdClose(&kbd);
+
+	return R_SUCCEEDED(rc);
+}
